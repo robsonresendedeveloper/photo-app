20f60efc9a0fb71d68dca84ffb6c5ce6bd628dea
Robson Resende <robson.souza.cl@gmail.com>
2017-07-06 18:11:50 +0000

    A lot of things that I didn't understand

--- Gemfile
+++ Gemfile
@@ -18,6 +18,8 @@ gem 'devise'
 gem 'twitter-bootstrap-rails'
 #Bootstrap for devise views
 gem 'devise-bootstrap-views'
+#Stripe for payments
+gem 'stripe'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170706144359) do
+ActiveRecord::Schema.define(version: 20170706171505) do
+
+  create_table "payments", force: :cascade do |t|
+    t.string   "email"
+    t.string   "token"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
